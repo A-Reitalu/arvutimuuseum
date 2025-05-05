e6265b283faba44ba4414bb2ebd9b05c869fdab5
legofoodjoker <arturreitalu2000@hotmail.com>
2025-05-05 11:24:06 +0300

    bug fixes

--- main.py
+++ main.py
@@ -4,35 +4,35 @@ import time
 import serial
 import music_player
 
-floppy_liigutamise_nupp = 14
+# floppy_liigutamise_nupp = 14
 Kõlari_eksponaadi_nupp = 15
-HDD_liigutamise_nupp = 18
-HDD_mootor1 = 17
-HDD_mootor2 = 27
-HDD_mootor_PWM = 24 
-HDD_pea1 = 22
-HDD_pea2 = 23
-HDD_pea_PWM = 25
+# HDD_liigutamise_nupp = 18
+# HDD_mootor1 = 17
+# HDD_mootor2 = 27
+# HDD_mootor_PWM = 24 
+# HDD_pea1 = 22
+# HDD_pea2 = 23
+# HDD_pea_PWM = 25
 
 #peame blokeerima floppy liigutamise eksponaadi selleks ajaks, kui sealt helifaile loetakse:
-floppy_lugemise_block = threading.lock()
+# floppy_lugemise_block = threading.Lock()
 
 GPIO.setmode(GPIO.BCM)
-#nupud:
-GPIO.setup(floppy_liigutamise_nupp, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# #nupud:
+# GPIO.setup(floppy_liigutamise_nupp, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(Kõlari_eksponaadi_nupp, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(HDD_liigutamise_nupp, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#HDD mootori liigutamine:
-GPIO.setup(HDD_mootor1, GPIO.OUT)
-GPIO.setup(HDD_mootor2, GPIO.OUT)
-GPIO.setup(HDD_mootor_PWM, GPIO.OUT)
-PWM_mootor = GPIO.PWM(HDD_mootor_PWM, 1000)
-#PWM_mootor.start(0)
-#HDD pea liigutamine:
-GPIO.setup(HDD_pea1, GPIO.OUT)
-GPIO.setup(HDD_pea2, GPIO.OUT)
-GPIO.setup(HDD_pea_PWM, GPIO.OUT)
-PWM_pea = GPIO.PWM(HDD_pea_PWM, 1000)
+# GPIO.setup(HDD_liigutamise_nupp, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# #HDD mootori liigutamine:
+# GPIO.setup(HDD_mootor1, GPIO.OUT)
+# GPIO.setup(HDD_mootor2, GPIO.OUT)
+# GPIO.setup(HDD_mootor_PWM, GPIO.OUT)
+# PWM_mootor = GPIO.PWM(HDD_mootor_PWM, 1000)
+# #PWM_mootor.start(0)
+# #HDD pea liigutamine:
+# GPIO.setup(HDD_pea1, GPIO.OUT)
+# GPIO.setup(HDD_pea2, GPIO.OUT)
+# GPIO.setup(HDD_pea_PWM, GPIO.OUT)
+# PWM_pea = GPIO.PWM(HDD_pea_PWM, 1000)
 #PWM_pea.start(0)
 
 
