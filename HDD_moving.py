06227fa51e28ca5a6bdb6e9666ac88e458c5318d
legofoodjoker <arturreitalu2000@hotmail.com>
2025-05-20 16:56:51 +0300

    loop fixes

--- HDD_moving.py
+++ HDD_moving.py
@@ -15,22 +15,22 @@ GPIO.setup(MOTOR_HEAD2, GPIO.OUT)
 GPIO.setup(ENA, GPIO.OUT)
 GPIO.setup(MOTOR_SPIN3, GPIO.OUT)
 GPIO.setup(MOTOR_SPIN4, GPIO.OUT)
-GPIO.setup(ENB, GPIO.OUT)
+#GPIO.setup(ENB, GPIO.OUT)
 
 PWM_A = GPIO.PWM(ENA, 1000)
 PWM_A.start(0) # Start with 0% duty cycle
-PWM_B = GPIO.PWM(ENB, 1000)
-PWM_B.start(100)
+#PWM_B = GPIO.PWM(ENB, 1000)
+#PWM_B.start(100)
 
 def MoveHDD():
     try:
         GPIO.output(MOTOR_SPIN3, GPIO.HIGH)
         GPIO.output(MOTOR_SPIN4, GPIO.LOW)
-        while True:
+        for i in range(5):
             # Motor forward
             GPIO.output(MOTOR_HEAD1, GPIO.HIGH)
             GPIO.output(MOTOR_HEAD2, GPIO.LOW)
-            PWM_A.ChangeDutyCycle(100)
+            PWM_A.ChangeDutyCycle(50)
             print("Motor running forward...")
             time.sleep(1)
 
@@ -44,7 +44,7 @@ def MoveHDD():
             # Motor backward (if you want to test direction)
             GPIO.output(MOTOR_HEAD1, GPIO.LOW)
             GPIO.output(MOTOR_HEAD2, GPIO.HIGH)
-            PWM_A.ChangeDutyCycle(100)
+            PWM_A.ChangeDutyCycle(50)
             print("Motor running backward...")
             time.sleep(1)
 
@@ -54,9 +54,17 @@ def MoveHDD():
             PWM_A.ChangeDutyCycle(0)
             print("Motor stopped.")
             time.sleep(2)
+        
 
     except KeyboardInterrupt:
         # Graceful exit
+        print("Stopping motor.")
+        GPIO.output(MOTOR_HEAD1, GPIO.LOW)
+        GPIO.output(MOTOR_HEAD2, GPIO.LOW)
+        GPIO.output(MOTOR_SPIN3, GPIO.LOW)
+        GPIO.output(MOTOR_SPIN4, GPIO.LOW)
+        GPIO.cleanup()
+    finally:
         print("Stopping motor.")
         GPIO.output(MOTOR_HEAD1, GPIO.LOW)
         GPIO.output(MOTOR_HEAD2, GPIO.LOW)
